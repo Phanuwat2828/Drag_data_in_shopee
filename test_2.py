6fa56c4d392827f5c1b20f9646600eb2c4c8071d
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-01-01 18:05:14 +0700

    0.0.88

--- test_2.py
+++ test_2.py
@@ -1,7 +1,6 @@
-import webbrowser;
+kai =  [1,2,3,4];
 
-url = 'https://www.youtube.com/watch?v=Lzqlp1iOBMQ';
-path = r'C:\Program Files\Google\Chrome\chrome.exe';
 
-webbrowser.register("chrome",None,webbrowser.BackgroundBrowser(path));
-webbrowser.get("chrome").open_new_tab(url);
+del kai[0];
+del kai[0];
+print(kai)
