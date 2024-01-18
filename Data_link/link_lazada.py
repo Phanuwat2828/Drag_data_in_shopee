bb07848185630b58a01927dd77869b9ffe0b8416
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-01-19 04:33:33 +0700

    2.0.9

--- Data_link/link_lazada.py
+++ Data_link/link_lazada.py
@@ -12,3 +12,20 @@ import shutil
 import keyboard as ky
 import pyperclip
 
+
+path_here = os.getcwd();
+json_file_path = path_here+'\Data_link\Data_link.json'
+def get_link():
+    with open(json_file_path, 'r', encoding='utf-8-sig') as file:
+        data = json.load(file)
+    return data;
+
+data = get_link();
+
+data_all = [];
+
+
+for data_1 in data['datalink']:
+    data_all.append(data_1.replace("https:////www.lazada.co.th/"));
+
+print(data_all)
